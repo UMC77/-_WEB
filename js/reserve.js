186b05d1272d554d3065aab8ace34c456fc25b9e
102sae <95170874+102sae@users.noreply.github.com>
2022-07-23 00:19:54 +0900

    예약 관리 페이지

--- js/reserve.js
+++ js/reserve.js
@@ -2,14 +2,22 @@ window.onload = function () {
   function onClick() {
     document.querySelector(".modal_wrap").style.display = "block";
     document.querySelector(".black_bg").style.display = "block";
+    document.body.style.overflow = "hidden";
   }
   function offClick() {
     document.querySelector(".modal_wrap").style.display = "none";
     document.querySelector(".black_bg").style.display = "none";
+    document.body.style.overflow = "auto";
+    document.querySelector(".modal_alert").style.display = "none";
+  }
+  function takeOrder() {
+    document.querySelector(".modal_alert").style.display = "block";
+    document.querySelector(".black_bg").style.display = "block";
+    document.querySelector(".modal_wrap").style.display = "none";
   }
 
   document.getElementById("modal_btn").addEventListener("click", onClick);
-  document.querySelector(".modal_take").addEventListener("click", offClick);
+  document.querySelector(".modal_take").addEventListener("click", takeOrder);
   document.querySelector(".modal_refuse").addEventListener("click", offClick);
-  document.querySelector(".modal_close").addEventListener("click", offClick);
+  document.querySelector(".modal_close2").addEventListener("click", offClick);
 };
