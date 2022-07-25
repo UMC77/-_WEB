2940244422c67dfadf38fe840e82578941750733
eunji <pih9134@naver.com>
2022-07-26 01:36:04 +0900

    review 수정중

---
+++ js/review.js
@@ -0,0 +1,6 @@
+function popup(){
+    var url = "popup.html";
+    var name = "popup test";
+    var option = "width = 500, height = 500, top = 100, left = 200, scrollbars = yes, location = no"
+    window.open(url, name, option);
+}
