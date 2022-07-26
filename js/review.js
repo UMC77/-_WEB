21d5e7cc2e5a7e9e3ad9ff71b0d07adb2bcc964f
eunji <pih9134@naver.com>
2022-07-27 02:44:57 +0900

    review ui틀  완성

--- js/review.js
+++ js/review.js
@@ -1,6 +1,16 @@
-function popup(){
-    var url = "popup.html";
-    var name = "popup test";
-    var option = "width = 500, height = 500, top = 100, left = 200, scrollbars = yes, location = no"
-    window.open(url, name, option);
-}
+window.onload=function(){
+    function onClick(){
+        document.querySelector('.modal_wrap').style.display='block';
+        document.querySelector('.black_bg').style.display='block';
+    }
+    function offClick(){
+        document.querySelector('.modal_wrap').style.display='none';
+        document.querySelector('.black_bg').style.display='none';
+    }
+        document.getElementById('modal_btn').addEventListener('click',onClick);
+        document.querySelector('.modal_close').addEventListener('click',offClick);
+    };
+
+    function goReserve(){
+        window.location.href = 'http://127.0.0.1:5502/handleReserve.html'
+    }
