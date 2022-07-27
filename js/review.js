843d84441d2c924c90a9f7ca1da9f13ef33e8f67
102sae <95170874+102sae@users.noreply.github.com>
2022-07-28 01:30:29 +0900

    충돌 수정

--- js/review.js
+++ js/review.js
@@ -1,14 +1,14 @@
 window.onload=function(){
     function onClick(){
-        document.querySelector('.modal_wrap').style.display='block';
+        document.querySelector('.modal_wrap_review').style.display='block';
         document.querySelector('.black_bg').style.display='block';
     }
     function offClick(){
-        document.querySelector('.modal_wrap').style.display='none';
+        document.querySelector('.modal_wrap_review').style.display='none';
         document.querySelector('.black_bg').style.display='none';
     }
         document.getElementById('modal_btn').addEventListener('click',onClick);
-        document.querySelector('.modal_close').addEventListener('click',offClick);
+        document.querySelector('.modal_close_review').addEventListener('click',offClick);
     };
 
     function goReserve(){
