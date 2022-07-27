c76779c9808bfbffdc59f4d63e93f97709d5d726
102sae <95170874+102sae@users.noreply.github.com>
2022-07-28 01:20:32 +0900

    메뉴 관리 기본

---
+++ js/goNext.js
@@ -0,0 +1,26 @@
+function goWait() {
+  window.location.href = "http://127.0.0.1:5502/handleReserve.html";
+}
+
+function goComplete() {
+  window.location.href = "http://127.0.0.1:5502/reserved.html";
+}
+function goAll() {
+  window.location.href = "http://127.0.0.1:5502/allReserved.html";
+}
+
+function goCancel() {
+  window.location.href = "http://127.0.0.1:5502/cancelReserved.html";
+}
+
+function goReserve() {
+  window.location.href = "http://127.0.0.1:5502/handleReserve.html";
+}
+
+function goReview() {
+  window.location.href = "http://127.0.0.1:5502/review.html";
+}
+
+function goMenu() {
+  window.location.href = "http://127.0.0.1:5502/handleMenu.html";
+}
--- js/reserve.js
+++ js/reserve.js
@@ -4,12 +4,14 @@ window.onload = function () {
     document.querySelector(".black_bg").style.display = "block";
     document.body.style.overflow = "hidden";
   }
+
   function offClick() {
     document.querySelector(".modal_wrap").style.display = "none";
     document.querySelector(".black_bg").style.display = "none";
     document.body.style.overflow = "auto";
     document.querySelector(".modal_alert").style.display = "none";
   }
+
   function takeOrder() {
     document.querySelector(".modal_alert").style.display = "block";
     document.querySelector(".black_bg").style.display = "block";
@@ -22,18 +24,3 @@ window.onload = function () {
   document.querySelector(".modal_close").addEventListener("click", offClick);
   document.querySelector(".modal_close2").addEventListener("click", offClick);
 };
-
-function goWait() {
-  window.location.href = "http://127.0.0.1:5502/handleReserve.html";
-}
-
-function goComplete() {
-  window.location.href = "http://127.0.0.1:5502/reserved.html";
-}
-function goAll() {
-  window.location.href = "http://127.0.0.1:5502/allReserved.html";
-}
-
-function goCancel() {
-  window.location.href = "http://127.0.0.1:5502/cancelReserved.html";
-}
