909b712bcee9aa686efa9fec737b97da6fe6525d
eunji <pih9134@naver.com>
2022-08-03 17:11:13 +0900

    가게정보 수정

--- js/review.js
+++ js/review.js
@@ -11,6 +11,22 @@ window.onload=function(){
         document.querySelector('.modal_close_review').addEventListener('click',offClick);
     };
 
+    // 예약관리
     function goReserve(){
         window.location.href = 'http://127.0.0.1:5502/handleReserve.html'
+    }
+
+    // 리뷰관리
+    function goReview(){
+        window.location.href = 'http://127.0.0.1:5502/review.html'
+    }
+
+    // 가게정보관리
+    function goStoreinfo(){
+        window.location.href = 'http://127.0.0.1:5502/storeinfo.html'
+    }
+
+    // 메뉴관리
+    function goMenu(){
+        window.location.href = 'http://127.0.0.1:5502/handleMenu.html'
     }
