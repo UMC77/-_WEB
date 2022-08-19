e197ef4344e9b0aaa4d89fb3b8bd46b90fbdba20
102sae <95170874+102sae@users.noreply.github.com>
2022-08-20 01:09:43 +0900

    header-nav, bottom-nav 추가

--- js/storeinfo.js
+++ js/storeinfo.js
@@ -4,6 +4,24 @@ $("#file").on("change", function () {
   $(".q1-box").val(fileName);
 });
 
+//textarea 크기 조절
 $("textarea.autosize").on("keydown keyup", function () {
   $(this).height(1).height($(this).prop("scrollHeight"));
 });
+
+//가게 정보 변수
+let storeName = document.querySelector(".storeName"); //가게 이름
+let StoreImg = document.querySelector("#file"); //가게 대표 이미지
+let ownNum = document.querySelector("#ownNum"); //사업자 번호
+let storeAddress = document.querySelector("#storeAddress"); //가게 주소
+let storeNum = document.querySelector("#storeNum"); //가게 전화번호
+
+let storeIntro = document.querySelector("#store_intro"); //가게 소개
+
+fetch("#")
+  .then((response) => response.json())
+  .then((data) => console.log(data));
+
+function checked(event) {
+  console.log(event.target);
+}
